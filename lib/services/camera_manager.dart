dd543f7eecd720e73e136e6f9e68870f2d9e095f
Omar Alam <omeralam7@gmail.com>
2022-01-22 01:57:55 -0500

    Create camera manager service

---
+++ lib/services/camera_manager.dart
@@ -0,0 +1,26 @@
+import 'package:oscw_mobile_app/widgets/remote_camera.dart';
+
+class RemoteCameraManager
+{
+
+  late int _numCams;
+  List _cameras = <RemoteCamera>[];
+
+  RemoteCameraManager._privateConstructor();
+
+  static final RemoteCameraManager _cameraManager = RemoteCameraManager._privateConstructor();
+
+  factory RemoteCameraManager()
+  {
+    return _cameraManager;
+  }
+
+  int get numCams => _numCams;
+
+  RemoteCamera getCamera(int index)
+  {
+    return (index >= 0 && index <_cameras.length ) ? _cameras[index] : null;
+  }
+
+
+}
